6f1a3c24aa696bd1b589b23d7a629e1dc0e6667b
bioshaolin <egetz@vt.edu>
2020-02-24 14:57:30 -0500

    endopep_peaks_v2.30

--- endopep_peaks/scripts/init_bash_dir/set_path.sh
+++ endopep_peaks/scripts/init_bash_dir/set_path.sh
@@ -3,6 +3,6 @@
 cd endopep_peaks_v2.30
 mv -f endopep_peaks ~
 cd ~
-rm -f endopep_peaks_v2.30
+rm -fr endopep_peaks_v2.30
 
 echo  "alias endopep_peaks='python ~/endopep_peaks/scripts/endopep.py'" >> ~/.bashrc
