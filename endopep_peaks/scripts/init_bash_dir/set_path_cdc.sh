c2a0e5e00940291d1075b6304c6263f1eb686315
bioshaolin <52865964+bioshaolin@users.noreply.github.com>
2020-04-08 15:51:15 -0400

    Update set_path_cdc.sh

--- endopep_peaks/scripts/init_bash_dir/set_path_cdc.sh
+++ endopep_peaks/scripts/init_bash_dir/set_path_cdc.sh
@@ -6,4 +6,4 @@ cd ~
 rm -fr endopep_peaks_v2.30
 
 echo  "alias endopep_peaks='python ~/endopep_peaks/scripts/endopep.py'" >> ~/.bashrc
-echo "alias init_cdc='source ~/endopep_peaks/scripts/init_bash/hpc_biolinux_module_init.sh'" >>~/.bashrc
+echo "alias init_cdc='source ~/endopep_peaks/scripts/init_bash_dir/hpc_biolinux_module_init.sh'" >>~/.bashrc
