55466b157c4545154662c72d89262a80df38c62b
bioshaolin <52865964+bioshaolin@users.noreply.github.com>
2020-05-13 14:16:56 -0400

    Update set_path.sh

--- endopep_peaks/scripts/init_bash_dir/set_path.sh
+++ endopep_peaks/scripts/init_bash_dir/set_path.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd endopep_peaks_v2.30
+cd endopep_peaks_v2.31
 mv -f endopep_peaks ~
 cd ~
-rm -fr endopep_peaks_v2.30
+rm -fr endopep_peaks_v2.31
 
 echo  "alias endopep_peaks='python ~/endopep_peaks/scripts/endopep.py'" >> ~/.bashrc
